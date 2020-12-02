a7c01ab2360c3d59408b2be537610a9bbd399527
Tsuyoshi-Toughy <ogutsuyoshi@gmail.com>
2020-12-02 11:35:03 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
+  # get '/users', to: 'users#index'
+  # get '/users/new', to: 'users#new'
+  # post '/users', to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
 
-  patch '/users/:id', to: 'users#update'
+  # patch '/users/:id', to: 'users#update'
 
-  delete '/users/:id', to: 'users#destroy'
+  # delete '/users/:id', to: 'users#destroy'
+
+  # get '/users/:id', to: 'users#show'
+  resources :users
 end
